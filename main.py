43163f7d835e38c875e700bfce238c8261f49bb4
romasi21 <matheusblanco21@gmail.com>
2025-08-01 12:58:17 +0000

    feat: Implementa motor de backtesting para simular trades

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ class Backtester:
         self.df = df.dropna()
         self.capital_inicial = capital_inicial
         self.capital = capital_inicial
+        self.n_acoes = 0
         self.trades = []
         self.posicao = False
 
@@ -15,7 +16,7 @@ class Backtester:
         for indice, linha in self.df.iterrows():
             self._checar_sinais(indice, linha)
         print("Backtest finalizado.")
-        print(f"Resultado final: R$ {self.capital:.2f}")
+        print(f"Resultado final: R$ {(self.capital + self.n_acoes * self.df['Close'].iloc[-1].item()):.2f}")
         
     def _checar_sinais(self, data, dia_atual): #compara as médias móveis para cada dia e gera um sinal se necessário
         posicao_atual = self.df.index.get_loc(data)
@@ -34,31 +35,35 @@ class Backtester:
     def _executar_compra(self, data, preco): #executa uma compra
         self.posicao = True
         self.trades.append({'data_compra': data, 'preco_compra': preco})
-
+        self.n_acoes = self.capital // preco
+        self.capital -= self.n_acoes * preco
+        
     def _executar_venda(self, data, preco): #executa uma venda
         self.posicao = False
         self.trades.append({'data_venda': data, 'preco_venda': preco})
+        self.capital += self.n_acoes * preco
+        self.n_acoes = 0
+        
 
 codigo_ativo = input("Insira o código de um ativo (ex: VALE3.SA): ")
 inicio = input("Início do período (ex: 2020-01-01): ")
 fim = input("Final do período (ex: 2023-12-31): ")
 
-
 dados = yf.download(codigo_ativo, start=inicio, end=fim)
 dados['SMA_curta'] = dados['Close'].rolling(window=21).mean()
 dados['SMA_longa'] = dados['Close'].rolling(window=50).mean()
-ativo_1 = Backtester(dados, 10000)
 
-print(ativo_1.df)
+ativo_1 = Backtester(dados, 10000)
 ativo_1.rodar_backtest()
 print(f"trades do ativo_1: {ativo_1.trades}")
 
-plt.plot(dados['Close'], label=f"Preco de fechamento ({codigo_ativo})")
-plt.plot(dados['SMA_curta'], label='Media movel curta')
-plt.plot(dados['SMA_longa'], label='Media movel longa')
+plt.plot(ativo_1.df['Close'], label=f"Preco de fechamento ({codigo_ativo})")
+plt.plot(ativo_1.df['SMA_curta'], label='Media movel curta')
+plt.plot(ativo_1.df['SMA_longa'], label='Media movel longa')
 
 plt.title('Preco de Fechamento vs Media Movel')
 plt.xlabel('Data')
+plt.xticks(rotation=45)
 plt.ylabel('Preco (R$)')
 plt.legend()
 plt.grid(True)
